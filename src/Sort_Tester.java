831f6df8855de7d7a63565ddbc79d35c4356e51f
Quinn Stevens <quinn.stevens@adacollege.org.uk>
2017-06-15 14:01:45 +0100

    did the code thing

--- src/Sort_Tester.java
+++ src/Sort_Tester.java
@@ -1,31 +1,63 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class Sort_Tester {
 
 
 
     public static void main(String[] args)
     {
+        // allow user to input custom array size
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How big is your array?");
+        int arraySize = scanner.nextInt();
+        int [] arr = new int [arraySize];
 
-        int [] arr = new int [100000];
+        // variables to keep track of start/finish times
+        long start;
+        long finish;
+        long elapsedTime;
 
+        Random random = new Random();
 
-        // TODO: generate random number and put them in the array here ...
+        // populate array
+        for (int i = 0; i < arraySize; i++) {
+            arr[i] = random.nextInt(50);
+        }
 
-
-
+        // create array to sort
+        int[] toBeSorted = new int[arr.length];
 
 
         Sort x = new Sort();
 
+        // copy random array so we can sort it without changing the state
+        // of the original array
+        System.arraycopy(arr, 0, toBeSorted, 0, arr.length);
+        start = System.nanoTime();
+        x.BubbleSort(toBeSorted);
+        finish = System.nanoTime();
+        elapsedTime = finish - start;
+        System.out.println("Bubble sort took: " + elapsedTime/1000000 + " milliseconds.");
 
-        // x.BubbleSort(arr);
+        // copy array again
+        System.arraycopy(arr, 0, toBeSorted, 0, arr.length);
+        start = System.nanoTime();
+        x.insertionSort(toBeSorted);
+        finish = System.nanoTime();
+        elapsedTime = finish - start;
+        System.out.println("Insertion sort took: " + elapsedTime/1000000 + " milliseconds.");
 
-        // x.insertionSort(arr);
+        // and again
+        System.arraycopy(arr, 0, toBeSorted, 0, arr.length);
+        start = System.nanoTime();
+        x.mergSort(toBeSorted, 0, toBeSorted.length);
+        finish = System.nanoTime();
+        elapsedTime = finish - start;
+        System.out.println("Merge sort took: " + elapsedTime/1000000 + " milliseconds.");
 
-        // x.mergSort(arr, 0, 100000);
-
+        System.arraycopy(toBeSorted, 0, arr, 0, toBeSorted.length);
         printArray(arr);
-
-
     }
 
     static void printArray(int a[])
