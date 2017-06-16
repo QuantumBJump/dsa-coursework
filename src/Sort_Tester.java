0d24b5faf9ac886afa0cd551fc5a7978b075b521
Quinn Stevens <quinn.stevens@adacollege.org.uk>
2017-06-16 15:33:55 +0100

    added result auto-generation

--- src/Sort_Tester.java
+++ src/Sort_Tester.java
@@ -1,70 +1,102 @@
+import com.sun.xml.internal.fastinfoset.tools.PrintTable;
+
+import java.io.File;
 import java.util.Scanner;
 import java.util.Random;
+import java.io.PrintWriter;
+import java.io.IOException;
+
 
 public class Sort_Tester {
+    static Random random = new Random();
+    static File outputfile = new File("results.csv");
+    static String outputString = "array size, bubble sort, insertion sort, merge sort, quick sort\n";
 
+    public static void write(File file, String output) {
+        try {
+            PrintWriter writer = new PrintWriter(file, "UTF-8");
+            writer.print(output);
+            writer.close();
+        } catch (IOException e) {
+            System.exit(1);
+        }
+    }
 
+    public static int[] generateArray(int size) {
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = random.nextInt(50);
+        }
+        return array;
+    }
 
-    public static void main(String[] args)
-    {
-        // allow user to input custom array size
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("How big is your array?");
-        int arraySize = scanner.nextInt();
-        int [] arr = new int [arraySize];
-
-        // variables to keep track of start/finish times
+    public static void sort(int[] array) {
+        // variables to keep track of times
         long start;
         long finish;
         long elapsedTime;
-
-        Random random = new Random();
-
-        // populate array
-        for (int i = 0; i < arraySize; i++) {
-            arr[i] = random.nextInt(50);
-        }
-
-        // create array to sort
-        int[] toBeSorted = new int[arr.length];
-
+        // copy of array so we can sort without changing original array
+        int[] toBeSorted = new int[array.length];
+        outputString = outputString + Integer.toString(array.length) + ", ";
 
         Sort x = new Sort();
 
-        // copy random array so we can sort it without changing the state
-        // of the original array
-        System.arraycopy(arr, 0, toBeSorted, 0, arr.length);
+        System.arraycopy(array, 0, toBeSorted, 0, array.length);
         start = System.nanoTime();
         x.BubbleSort(toBeSorted);
         finish = System.nanoTime();
         elapsedTime = finish - start;
-        System.out.println("Bubble sort took: " + elapsedTime/1000000 + " milliseconds.");
+        outputString = outputString + Long.toString(elapsedTime/1000000) + ", ";
 
         // copy array again
-        System.arraycopy(arr, 0, toBeSorted, 0, arr.length);
+        System.arraycopy(array, 0, toBeSorted, 0, array.length);
         start = System.nanoTime();
         x.insertionSort(toBeSorted);
         finish = System.nanoTime();
         elapsedTime = finish - start;
-        System.out.println("Insertion sort took: " + elapsedTime/1000000 + " milliseconds.");
+        outputString = outputString + Long.toString(elapsedTime/1000000) + ", ";
 
         // and again
-        System.arraycopy(arr, 0, toBeSorted, 0, arr.length);
+        System.arraycopy(array, 0, toBeSorted, 0, array.length);
         start = System.nanoTime();
         x.mergeSort(toBeSorted, 0, toBeSorted.length);
         finish = System.nanoTime();
         elapsedTime = finish - start;
-        System.out.println("Merge sort took: " + elapsedTime/1000000 + " milliseconds.");
+        outputString = outputString + Long.toString(elapsedTime/1000000) + ", ";
 
-        System.arraycopy(arr, 0, toBeSorted, 0, arr.length);
+        System.arraycopy(array, 0, toBeSorted, 0, array.length);
         start = System.nanoTime();
         x.quickSort(toBeSorted, 0, toBeSorted.length-1);
         finish = System.nanoTime();
         elapsedTime = finish - start;
-        System.out.println("Quick sort took: " + elapsedTime/1000000 + " milliseconds.");
+        outputString = outputString + Long.toString(elapsedTime/1000000) + "\n";
+    }
 
-        System.arraycopy(toBeSorted, 0, arr, 0, toBeSorted.length);
-//        printArray(arr);
+    public static void main(String[] args)
+    {
+        // allow user to input custom array size
+
+        int[] arraySizes = {100, 1000, 10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000};
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("How big is your array?");
+//        int arraySize = scanner.nextInt();
+//        int [] arr = new int [arraySize];
+
+        // reset output file
+        outputfile.delete();
+
+        for (int i = 0; i < arraySizes.length; i++) {
+            int[] array = generateArray(arraySizes[i]);
+            sort(array);
+        }
+
+        try {
+            PrintWriter writer = new PrintWriter(outputfile, "UTF-8");
+            writer.print(outputString);
+            writer.close();
+        } catch (IOException e) {
+            System.exit(1);
+        }
     }
 
     static void printArray(int a[])
